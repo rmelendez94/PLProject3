36306eefcf43c96a52f8f591010ee07e460463fc
Rolando Melendez <rolando.melendez@utexas.edu>
2016-03-06 22:17:54 -0600

    Final Push project DONE! (most likely) just kidding, it really is.

--- Problem 3 Java Rework/person_tester.js
+++ Problem 3 Java Rework/person_tester.js
@@ -92,59 +92,128 @@ var employee = function() {
 
 // JQUERY STUFF HERE
 $(document).ready(function() {
-    alert("Welcome to the Person Tester Application!\nAs you use the application, please be sure to fill in all available fields before clicking enter.");
-
-    $("#enter").click(function () {
-        // You chose customer
-        if ($("#customer_choice").is(":checked") === true && $("#employee_choice").is(":checked") === false) {
-            // Toggle hide after submitting enter
-            $("#choice").toggleClass("hide");
-            $("#customer").toggleClass("hide");
+    alert("Welcome to the Person Tester Application!\nAs you use the application, please be sure to fill in all available fields before submitting enter.");
 
 
-            $("#enter").click(function() {
-                var fncustomer = $("#first_name_customer").val();
-                var lncustomer = $("#last_name_customer").val();
-                var eacustomer = $("#email_address_customer").val();
-                var customernum = $("#customer_number").val();
+    var formValidation = {
+        choice: function() {
+            if ($("#customer_choice").is(":checked") === true && $("#employee_choice").is(":checked") === false) {
+                // Toggle hide after submitting enter
+                $("#choice").toggleClass("hide");
+                $("#customer").toggleClass("hide");
+                return "customer"
+            }
+            else if ($("#customer_choice").is(":checked") === false && $("#employee_choice").is(":checked") === true) {
+                // Toggle hide after submitting enter
+                $("#choice").toggleClass("hide");
+                $("#employee").toggleClass("hide");
+                return "employee"
+            }
+            else {
+                alert("Please make a selection.")
+                return false;
+            }
+        },
+        customer: function () {
+            var fncustomer = $("#first_name_customer").val();
+            var lncustomer = $("#last_name_customer").val();
+            var eacustomer = $("#email_address_customer").val();
+            var customernum = $("#customer_number").val();
 
-                // check if all fields are populated and valid
-                if (fncustomer != "" && lncustomer != "" && eacustomer != "" && customernum != "") {
-                    // show output
-                    alert("You got here")
+            if (fncustomer && lncustomer && eacustomer && customernum){
+
+                if (!eacustomer.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$')) {
+                    alert("Please input a valid email address");
+                    return false;
                 }
-                else {
-                    alert("Please fill in all fields before submitting.");
-                }
-            })
 
-        }
-        // You chose employee
-        else if ($("#customer_choice").is(":checked") === false && $("#employee_choice").is(":checked") === true) {
-            // Toggle hide after submitting enter
-            $("#choice").toggleClass("hide");
-            $("#employee").toggleClass("hide");
+                // hide customer input fields
+                $("#customer").toggleClass("hide");
 
-            $("#enter").click(function() {
-                var fnemployee = $("#first_name_employee").val();
-                var lnemployee = $("#last_name_employee").val();
-                var eaemployee = $("#email_address_employee").val();
-                var ssn = $("#social_security").val();
+                // show output
+                $("#output").toggleClass("hide");
+                $("#output_paragraph").html('You entered:<br>Name: ' + fncustomer + ' ' + lncustomer + '<br>Email: ' + eacustomer + '<br>Customer number: ' + customernum);
 
-                // check if all fields are populated and valid
-                if (fnemployee != "" && lnemployee != "" && eaemployee != "" && ssn != "") {
-                    // show output
-                    alert("you also got here")
-                }
-                else {
-                    alert("Please fill in all fields before submitting.");
-                }
-            })
+                // show question to continue
+                $("#continue_choice").toggleClass("hide");
+                return true;
+            }
+            else {
+                alert("Please fill in all fields before submitting.")
+                return false;
+            }
+        },
+        employee: function() {
+            var fnemployee = $("#first_name_employee").val();
+            var lnemployee = $("#last_name_employee").val();
+            var eaemployee = $("#email_address_employee").val();
+            var ssn = $("#social_security").val();
+
             // check if all fields are populated and valid
-        }
-        else {
-            alert("Please make a selection.");
+            if (fnemployee && lnemployee && eaemployee && ssn) {
+
+                if (!eaemployee.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$')) {
+                    alert("Please input a valid email address.");
+                    return false;
+                }
+
+                if (!ssn.match('^([0-9]{3}-?[0-9]{2}-?[0-9]{4})$')) {
+                    alert("Please enter a valid social security number.");
+                    return false;
+                }
+
+                // hide employee input fields
+                $("#employee").toggleClass("hide");
+
+                // show output
+                $("#output").toggleClass("hide");
+                $("#output_paragraph").html("You entered:<br>Name: " + fnemployee + " " + lnemployee + "<br>Email: " + eaemployee + "<br>Social security number: " + ssn);
+
+                // show question to continue
+                $("#continue_choice").toggleClass("hide");
+            }
+            else {
+                alert("Please fill in all fields before submitting.");
+                return false;
+            }
+        },
+        continueloop: function() {
+            if ($("#continue_yes").is(":checked") == true && $("#continue_no").is(":checked") == false) {
+                // close window
+                location.reload();
+                return "yes"
+            }
+            else if ($("#continue_yes").is(":checked") == false && $("#continue_no").is(":checked") == true) {
+                // reload window
+                alert("Thanks for stopping by!");
+                window.close();
+                return "no"
+            }
+            else {
+                alert("Please make a selection.");
+                return false;
+            }
         }
 
+    }
+    $("#enter_choice").click(function ( event ) {
+        formValidation.choice();
+        event.preventDefault();
+    })
+
+    $("#enter_customer").click(function ( event ) {
+        formValidation.customer();
+        event.preventDefault();
+    })
+
+    $("#enter_employee").click(function ( event ) {
+        formValidation.employee();
+        event.preventDefault();
+    })
+
+
+    $("#enter_continue").click(function ( event ) {
+        formValidation.continueloop();
+        event.preventDefault();
     })
 });
